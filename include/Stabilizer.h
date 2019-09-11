8e1bf3d8fff3e977a0644f52280a6b73f88d9bd5
Boris Makaev <b.makaev@innopolis.ru>
2019-09-11 23:19:18 +0300

    Fix grid allocation and remove crop for now

--- include/Stabilizer.h
+++ include/Stabilizer.h
@@ -9,6 +9,7 @@
 
 using cv::Mat;
 using cv::VideoCapture;
+using cv::Point2f;
 
 namespace meshflow {
 
@@ -16,7 +17,14 @@ namespace meshflow {
 class Stabilizer
 {
 public:
+    Stabilizer(const std::string& filename, cv::Size sz) : filename(filename),
+                                                           PIXELS_X(sz.width / 16),
+                                                           PIXELS_Y(sz.height / 16) {};
+
     const int PIXELS = 16;
+    int PIXELS_X;
+    int PIXELS_Y;
+
     const int RADIUS = 300;
 
     int HORIZONTAL_BORDER;
@@ -30,11 +38,24 @@ public:
 
     void generateStabilizedVideo(VideoCapture& cap);
 
+    void extractFeaturesFromGrid(const Mat& frame, std::vector<cv::Point2f>& corners);
+
+    void simpleRejection(const vector<uchar>& status, const vector<Point2f>& frameCorners, const vector<Point2f>& newFrameCorners,
+                         vector<Point2d>& old_points, vector<Point2d>& new_points);
+
+    void outlierRejection(const vector<uchar>& status, const vector<Point2f>& frameCorners, const vector<Point2f>& newFrameCorners,
+                          vector<Point2d>& old_points, vector<Point2d>& new_points);
 private:
+    std::string filename;
+
     std::vector<cv::Mat> smoothXPaths, smoothYPaths;
     std::vector<cv::Mat> xPaths, yPaths;
     std::vector<cv::Mat> xMotionMeshes, yMotionMeshes;
     std::vector<cv::Mat> newXMotionMeshes, newYMotionMeshes;
+
+    std::vector<std::pair<double, double>> lambdas;
+
+    double width, height, cellHeight, cellWidth;
 
 };
 
--- src/Stabilizer.cpp
+++ src/Stabilizer.cpp
@@ -12,15 +12,22 @@ void Stabilizer::readVideo(VideoCapture& cap) {
     cv::cvtColor(oldFrame, oldGray, cv::COLOR_BGR2GRAY);
     int frameCount = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_COUNT));
 
+    width = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH));
+    height = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+    cellWidth = width / 4;
+    cellHeight = width / 4;
+
     // Preserve aspect ratio
     HORIZONTAL_BORDER = 30;
     VERTICAL_BORDER = (HORIZONTAL_BORDER * oldGray.cols) / oldGray.rows;
 
     // motion meshes in x-direction and y-direction
-    xPaths.push_back(Mat::zeros(oldFrame.rows/PIXELS, oldFrame.cols/PIXELS, CV_64F));
-    yPaths.push_back(Mat::zeros(oldFrame.rows/PIXELS, oldFrame.cols/PIXELS, CV_64F));
+    xPaths.push_back(Mat::zeros(oldFrame.rows/PIXELS_Y + 1, oldFrame.cols/PIXELS_X + 1, CV_64F));
+    yPaths.push_back(Mat::zeros(oldFrame.rows/PIXELS_Y + 1, oldFrame.cols/PIXELS_X + 1, CV_64F));
 
     int frameNum{1};
+
+    lambdas.push_back(std::make_pair(1, 1));
 
     while (frameNum < frameCount) {
         Mat frame, frameGray;
@@ -28,28 +35,26 @@ void Stabilizer::readVideo(VideoCapture& cap) {
         cv::cvtColor(frame, frameGray, cv::COLOR_BGR2GRAY);
 
         std::vector<cv::Point2f> p0, p1;
-        cv::goodFeaturesToTrack(oldGray, p0, 1000, 0.3, 7, Mat(), 7);
+        extractFeaturesFromGrid(oldGray, p0); // rich feature extraction
+        //cv::goodFeaturesToTrack(oldGray, p0, 1000, 0.3, 7, Mat(), 7);
 
         std::vector<uchar> status;
         std::vector<float> err;
         cv::calcOpticalFlowPyrLK(oldGray, frameGray, p0, p1, status, err, cv::Size(15, 15), 2, cv::TermCriteria(cv::TermCriteria::EPS | cv::TermCriteria::COUNT, 20, 0.03));
 
-        // select good points
+        /// select good points: outlier rejection
         std::vector<cv::Point2d> goodOld, goodNew;
-
-        for (int i = 0; i < status.size(); ++i) {
-            if (status[i] == 1) {
-                goodNew.push_back(p1[i]);
-                goodOld.push_back(p0[i]);
-            }
-        }
+        outlierRejection(status, p0, p1, goodOld, goodNew);
+        //simpleRejection(status, p0, p1, goodOld, goodNew);
 
         // Estimate motion mesh for old_frame
-        Mat xMotionMesh, yMotionMesh;
-        MeshFlow::motionPropagate(goodOld, goodNew, frame, xMotionMesh, yMotionMesh);
+        Mat xMotionMesh = Mat::zeros(oldFrame.rows/PIXELS_Y + 1, oldFrame.cols/PIXELS_X + 1, CV_64F),
+            yMotionMesh = Mat::zeros(oldFrame.rows/PIXELS_Y + 1, oldFrame.cols/PIXELS_X + 1, CV_64F);
+
+        MeshFlow::motionPropagate(goodOld, goodNew, frame, xMotionMesh, yMotionMesh, lambdas, cv::Size(PIXELS_X, PIXELS_Y));
         xMotionMeshes.push_back(xMotionMesh);
         yMotionMeshes.push_back(yMotionMesh);
-
+        //Log(xMotionMesh);
         // Generate vertex profiles
         MeshFlow::generateVertexProfiles(xMotionMesh, yMotionMesh, xPaths, yPaths);
 
@@ -62,16 +67,16 @@ void Stabilizer::readVideo(VideoCapture& cap) {
 }
 
 void Stabilizer::stabilize(bool online) {
-    // std::cout << xPaths[2] << std::endl;
+
     if (online) {
         Optimizer::onlinePathOptimization(xPaths, smoothXPaths, 30);
         Log("smoothX!!!");
         Optimizer::onlinePathOptimization(yPaths, smoothYPaths, 30);
         Log("smoothY!!!");
     } else {
-        Optimizer::offlinePathOptimization(xPaths, smoothXPaths);
+        Optimizer::offlinePathOptimization(xPaths, smoothXPaths, lambdas);
         Log("smoothX!!!");
-        Optimizer::offlinePathOptimization(yPaths, smoothYPaths);
+        Optimizer::offlinePathOptimization(yPaths, smoothYPaths, lambdas);
         Log("smoothY!!!");
     }
 
@@ -86,7 +91,7 @@ void Stabilizer::getFrameWarp()
     xMotionMeshes.push_back(xMotionMeshes.back());
     yMotionMeshes.push_back(yMotionMeshes.back());
 
-    for (int i = 0; i < xPaths.size(); ++i) {
+    for (size_t i = 0; i < xPaths.size(); ++i) {
         newXMotionMeshes.push_back(smoothXPaths[i] - xPaths[i]);
         newYMotionMeshes.push_back(smoothYPaths[i] - yPaths[i]);
     }
@@ -103,7 +108,7 @@ void Stabilizer::generateStabilizedVideo(VideoCapture &cap)
     int frameCount = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_COUNT));
 
     // get video parameters
-    cv::VideoWriter out = cv::VideoWriter("../stable.avi", CV_FOURCC('X', 'V', 'I', 'D'), frameRate, cv::Size(frameWidth*2, frameHeight));
+    cv::VideoWriter out = cv::VideoWriter(filename+"_stable.avi", CV_FOURCC('M', 'J', 'P', 'G'), frameRate, cv::Size(frameWidth*2, frameHeight));
 
     int frameNum{0};
     while (frameNum < frameCount) {
@@ -118,14 +123,14 @@ void Stabilizer::generateStabilizedVideo(VideoCapture &cap)
 
         // Mesh warping
         Mat newFrame;
-        MeshFlow::meshWarpFrame(frame, newXMotionMesh, newYMotionMesh, newFrame);
+        MeshFlow::meshWarpFrame(frame, newXMotionMesh, newYMotionMesh, newFrame, cv::Size(PIXELS_X, PIXELS_Y));
         // new_frame = new_frame[HORIZONTAL_BORDER:-HORIZONTAL_BORDER, VERTICAL_BORDER:-VERTICAL_BORDER, :]
 
         /// resize image - apply crop (uncomment)
-        newFrame = newFrame(cv::Range(HORIZONTAL_BORDER, frame.rows-HORIZONTAL_BORDER), cv::Range(VERTICAL_BORDER, frame.cols-VERTICAL_BORDER));
-        cv::resize(newFrame, newFrame, cv::Size(frame.cols, frame.rows), cv::INTER_CUBIC);
+        //newFrame = newFrame(cv::Range(HORIZONTAL_BORDER, frame.rows-HORIZONTAL_BORDER), cv::Range(VERTICAL_BORDER, frame.cols-VERTICAL_BORDER));
+        //cv::resize(newFrame, newFrame, cv::Size(frame.cols, frame.rows), cv::INTER_CUBIC);
 
-        // Write images
+        /// Concatenate both images
         Mat outputFrame(frameHeight, 2*frameWidth, frame.type());
         cv::hconcat(frame, newFrame, outputFrame);
 
@@ -140,6 +145,105 @@ void Stabilizer::generateStabilizedVideo(VideoCapture &cap)
     cap.release();
     out.release();
 
+}
+
+void Stabilizer::extractFeaturesFromGrid(const Mat &frame, std::vector<cv::Point2f>& corners) {
+    vector<Mat> grid;
+    int i{0};
+    vector<cv::Point2f> cellCorners[16], newFrameCorners;
+
+    // The part which extract features for each grid cell
+    for (int y = 0; y <= height - cellHeight; y += cellHeight)
+    {
+        for (int x = 0; x <= width - cellWidth; x += cellWidth)
+        {
+
+            cv::Rect region;
+            region.x = x; region.y = y;
+            region.width = static_cast<int>(cellWidth);
+            region.height = static_cast<int>(cellHeight);
+
+            Mat cell = frame(region);
+            grid.push_back(cell);
+
+            goodFeaturesToTrack(cell, cellCorners[i], 200, 0.001, 7, Mat(), 3);
+
+            // Fix points to account cell coordinates w.r.t. image coordinates
+            for(auto & j : cellCorners[i])
+            {
+                j = cv::Point2f(j.x + x, j.y + y);
+            }
+
+
+            // Now get all points in one array
+            corners.insert(corners.end(), cellCorners[i].begin(), cellCorners[i].end());
+            i++;
+        }
+    }
+}
+
+void Stabilizer::simpleRejection(const vector<uchar> &status, const vector<Point2f> &frameCorners,
+                                 const vector<Point2f> &newFrameCorners,
+                                 vector<Point2d> &old_points, vector<Point2d> &new_points) {
+
+    for (int i = 0; i < status.size(); ++i) {
+        if (status[i] == 1) {
+            new_points.push_back(newFrameCorners[i]);
+            old_points.push_back(frameCorners[i]);
+        }
+    }
+}
+
+
+void Stabilizer::outlierRejection(const vector<uchar> &status, const vector<Point2f> &frameCorners,
+                                  const vector<Point2f> &newFrameCorners,
+                                  vector<Point2d> &old_points, vector<Point2d> &new_points) {
+    // Brush off invalid points after optical flow
+    vector<Point2d> goodNew_temp, goodOld_temp;
+    for (int i = 0; i < status.size(); ++i) {
+        if (status[i] == 1) {
+            goodNew_temp.push_back(newFrameCorners[i]);
+            goodOld_temp.push_back(frameCorners[i]);
+        }
+    }
+
+    int i{0};
+    // TODO: Parallelize code
+    for (int y = 0; y <= height - cellHeight; y += cellHeight) {
+        for (int x = 0; x <= width - cellWidth; x += cellWidth) {
+
+            cv::Rect region;
+            region.x = x; region.y = y;
+            region.width = static_cast<int>(cellWidth);
+            region.height = static_cast<int>(cellHeight);
+
+            vector<Point2d> binOldPoints, binNewPoints;
+            for (size_t i = 0; i < goodOld_temp.size(); ++i)
+            {
+                if(region.contains(goodOld_temp[i]) && region.contains(goodNew_temp[i])) {
+                    binOldPoints.push_back(goodOld_temp[i]);
+                    binNewPoints.push_back(goodNew_temp[i]);
+                }
+            }
+
+            vector<uchar> status_new;
+            Mat H = findHomography(binOldPoints, binNewPoints, status_new, cv::RANSAC, 2);
+
+            vector<Point2f> cleanOldPoints, cleanNewPoints;
+            for (size_t i = 0; i < binOldPoints.size(); ++i)
+            {
+                if (status_new[i]) {
+                    cleanOldPoints.push_back(binOldPoints[i]);
+                    cleanNewPoints.push_back(binNewPoints[i]);
+                }
+            }
+
+            // Now get all points in one array
+            old_points.insert(old_points.end(), cleanOldPoints.begin(), cleanOldPoints.end());
+            new_points.insert(new_points.end(), cleanNewPoints.begin(), cleanNewPoints.end());
+            i++;
+        }
+    }
 }
 
 }
